6963c8981ba41ccbd96269c6da6aa8850e256524
calebetadeu@gmail.com <calebetadeu@gmail.com>
2025-02-09 09:43:03 -0300

    Corrige caminho do sistema no env.py

--- tidy_api/alembic/env.py
+++ tidy_api/alembic/env.py
@@ -5,11 +5,10 @@ import os
 import sys
 
 # Adicione o caminho do seu aplicativo ao sys.path
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'app'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 
 # Importa o modelo Base do seu aplicativo
-from app.models import Base
-
+from tidy_api.models import Base
 
 # Configurações do arquivo alembic.ini
 config = context.config
